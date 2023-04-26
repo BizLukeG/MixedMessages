8136fbc0e1cb2a08f3144ca8ea10622324a63a0f
Luke Gor <BizLukeG@gmail.com>
2023-04-26 18:21:04 -0500

    Update README explaining purpose

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,8 +1,8 @@
-teamNames = ["Cowboys", "Bills", "Ravens", "Falcons", "Panthers", "Jets"];
+const teamNames = ["Cowboys", "Bills", "Ravens", "Falcons", "Panthers", "Jets"];
 
-prospects = ["Anthony Richardson", "Bryce Young", "Bijan Robinson", "Jahmyr Gibbs", "Zay Flowers"];
+const prospects = ["Anthony Richardson", "Bryce Young", "Bijan Robinson", "Jahmyr Gibbs", "Zay Flowers"];
 
-draftPick = ["1st", "2nd", "3rd", "4th", "5th", "6th"];
+const draftPick = ["1st", "2nd", "3rd", "4th", "5th", "6th"];
 
 function getRandomArrayValue(array){
   return array[Math.floor(Math.random() * array.length)];
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-This is my Mixed Messages Project
+#Mixed Messages Project
+
+This Project was created to generate a random
+* Team
+* Prospect
+* Selection
+combination that could occur in the 2023 NFL Draft, and put it into a message.
