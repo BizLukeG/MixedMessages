38541f824c5284c9415702c157863d2512469e88
Luke Gor <BizLukeG@gmail.com>
2023-04-26 18:05:42 -0500

    add function to create random message

--- MixedMessages.js
+++ MixedMessages.js
@@ -3,3 +3,10 @@ teamNames = ["Cowboys", "Bills", "Ravens", "Falcons", "Panthers", "Jets"];
 prospects = ["Anthony Richardson", "Bryce Young", "Bijan Robinson", "Jahmyr Gibbs", "Zay Flowers"];
 
 draftPick = ["1st", "2nd", "3rd", "4th", "5th", "6th"];
+
+function getRandomArrayValue(array){
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+console.log(`With the ${getRandomArrayValue(draftPick)} overall draft pick in the 2023 NFL draft,
+the ${getRandomArrayValue(teamNames)} select ${getRandomArrayValue(prospects)}!`)
