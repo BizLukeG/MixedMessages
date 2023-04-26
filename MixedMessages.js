f2fc55be8bb155127d9eb6d671cff2b18eeeddec
Luke Gor <BizLukeG@gmail.com>
2023-04-26 17:46:10 -0500

    add teamNames prospects and draftPicks

---
+++ MixedMessages.js
@@ -0,0 +1,5 @@
+teamNames = ["Cowboys", "Bills", "Ravens", "Falcons", "Panthers", "Jets"];
+
+prospects = ["Anthony Richardson", "Bryce Young", "Bijan Robinson", "Jahmyr Gibbs", "Zay Flowers"];
+
+draftPick = ["1st", "2nd", "3rd", "4th", "5th", "6th"];
